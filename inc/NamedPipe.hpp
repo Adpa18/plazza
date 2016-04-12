7ab24d38964a6275c8c340d88a3403678b3a536b
Adrien WERY <adrien.wery@epitech.eu>
2016-04-12 15:44:51 +0200

    NamedPipe

--- inc/NamedPipe.hpp
+++ inc/NamedPipe.hpp
@@ -1,13 +1,34 @@
 #ifndef NAMEDPIPE_HPP
 # define NAMEDPIPE_HPP
 
+# include <string>
+# include <iostream>
+# include <sstream>
+# include <fstream>
+# include <fcntl.h>
+# include <sys/stat.h>
+# include <unistd.h>
+
 class NamedPipe {
 private:
-    /* data */
+    const std::string   fileName;
 public:
-    NamedPipe ();
+    NamedPipe (const std::string &fileName);
     virtual ~NamedPipe ();
-
+    std::string     getFileName(void) const;
+    void            write(const std::string &str) const;
+    std::string     read() const;
+    void            operator>>(std::ostream &out);
 };
+
+std::ostream    &operator<<(std::ostream &stream, const NamedPipe &obj);
+template <typename X>
+void            operator>>(X stream, const NamedPipe &obj)
+{
+    std::stringstream   ss;
+
+    ss << stream;
+    obj.write(ss.str());
+}
 
 #endif /* !NAMEDPIPE_HPP */
--- src/NamedPipe.cpp
+++ src/NamedPipe.cpp
@@ -1,11 +1,43 @@
 #include "NamedPipe.hpp"
 
-NamedPipe::NamedPipe()
+NamedPipe::NamedPipe(const std::string &fileName) : fileName(fileName + ".fifo")
 {
-
+    mkfifo(this->fileName.c_str(), S_IWUSR | S_IRUSR | S_IRGRP);
 }
 
 NamedPipe::~NamedPipe()
 {
+    unlink(this->fileName.c_str());
+}
 
+std::string     NamedPipe::getFileName(void) const
+{
+    return (this->fileName);
+}
+
+void            NamedPipe::write(const std::string &str) const
+{
+    std::ofstream       file;
+
+    file.open(this->getFileName());
+    file << str;
+    file.close();
+}
+
+std::string     NamedPipe::read() const
+{
+    std::ifstream   file;
+    std::string     str;
+
+    file.open(this->getFileName());
+    str = std::string((std::istreambuf_iterator<char>(file)),
+                 std::istreambuf_iterator<char>());
+
+    file.close();
+    return (str);
+}
+
+std::ostream    &operator<<(std::ostream &stream, const NamedPipe &obj)
+{
+    return (stream << obj.read());
 }
