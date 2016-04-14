9ff623c2c21cb6e924e03253b9477d1125987c9b
vencat_a <vencat_a@epitech.eu>
2016-04-14 16:20:47 +0200

    parser init

--- inc/Plazza.hpp
+++ inc/Plazza.hpp
@@ -1,9 +1,17 @@
 #ifndef PLAZZA_HPP
 # define PLAZZA_HPP
 
+# include <vector>
+# include <stack>
+# include <utility>
+# include <string>
+
+enum CMD { PHONE_NUMBER, EMAIL, IP_ADRESS };
+
 class Plazza {
 private:
     unsigned int    nbThreads;
+    std::vector<std::pair<CMD, std::stack<std::string>>> orders;
 public:
     Plazza (unsigned int nbThreads);
     virtual ~Plazza ();
