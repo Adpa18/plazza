177f3ce013e0d61cc2d1953e769c97c882513f54
vencat_a <vencat_a@epitech.eu>
2016-04-16 20:58:50 +0200

    parser updated and finalised

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,10 +1,20 @@
 #include "Plazza.hpp"
 #include <iostream>
 
+void		supp_space(std::string &str)
+{
+    size_t	size;
+
+    while ((size = str.find(" ")) != std::string::npos) {
+        str = str.erase(size);
+    }
+}
+
 std::vector<std::pair<Information, std::stack<std::string> > > parse(std::string &cmd)
 {
     std::vector<std::pair<Information, std::stack<std::string> > >	ret;
     std::string														tmp;
+    std::string														tmp_sub;
     std::string														cur_cmd;
     size_t															size;
     size_t															size_tmp;
@@ -18,16 +28,28 @@ std::vector<std::pair<Information, std::stack<std::string> > > parse(std::string
         tmp = cmd.substr(0, size);
         cur_cmd = tmp.substr(0, tmp.find(" ") == std::string::npos ? tmp.size() : tmp.find(" "));
         tmp = tmp.substr(tmp.find(" ") == std::string::npos ? tmp.size() : tmp.find(" "));
-        while ((size_tmp = tmp.find(" ")) != std::string::npos) {
-            sta.push(tmp.substr(size_tmp));
-            tmp = tmp.substr(size_tmp + 1);
+        while (tmp.find(" ") == 0) {
+            tmp = tmp.substr(1);
         }
+        while ((size_tmp = tmp.find(" ")) != std::string::npos) {
+            tmp_sub = tmp.substr(size_tmp);
+            supp_space(tmp_sub);
+            sta.push(tmp_sub);
+            tmp = tmp.substr(size_tmp + 1);
+            while (tmp.find(" ") == 0) {
+                tmp = tmp.substr(1);
+            }
+        }
+        while (tmp.find(" ") == 0) {
+            tmp = tmp.substr(1);
+        }
+        supp_space(tmp);
         sta.push(tmp);
-        if (cur_cmd == "EMAIL_ADDRESS") {
+        if (cur_cmd == "EMAIL_ADDRESS" && !sta.empty()) {
             ret.push_back(std::pair<Information, std::stack<std::string> >(EMAIL_ADDRESS, sta));
-        } else if (cur_cmd == "IP_ADDRESS") {
+        } else if (cur_cmd == "IP_ADDRESS" && !sta.empty()) {
             ret.push_back(std::pair<Information, std::stack<std::string> >(IP_ADDRESS, sta));
-        } else if (cur_cmd == "PHONE_NUMBER") {
+        } else if (cur_cmd == "PHONE_NUMBER" && !sta.empty()) {
             ret.push_back(std::pair<Information, std::stack<std::string> >(PHONE_NUMBER, sta));
         }
         std::cout << "in : tmp = \"" << tmp << "\"" << std::endl << "cur_cmd = \"" << cur_cmd << "\"" << std::endl;
@@ -40,12 +62,29 @@ std::vector<std::pair<Information, std::stack<std::string> > > parse(std::string
     }
     tmp = cmd;
     cur_cmd = tmp.substr(0, tmp.find(" ") == std::string::npos ? tmp.size() : tmp.find(" "));
-    sta.push(tmp.substr(tmp.find(" ") == std::string::npos ? tmp.size() : tmp.find(" ")));
-    if (cur_cmd == "EMAIL_ADRESS") {
+    tmp = tmp.substr(tmp.find(" ") == std::string::npos ? tmp.size() : tmp.find(" "));
+    while (tmp.find(" ") == 0) {
+        tmp = tmp.substr(1);
+    }
+    while ((size_tmp = tmp.find(" ")) != std::string::npos) {
+        tmp_sub = tmp.substr(size_tmp);
+        supp_space(tmp_sub);
+        sta.push(tmp_sub);
+        tmp = tmp.substr(size_tmp + 1);
+        while (tmp.find(" ") == 0) {
+            tmp = tmp.substr(1);
+        }
+    }
+    while (tmp.find(" ") == 0) {
+        tmp = tmp.substr(1);
+    }
+    supp_space(tmp);
+    sta.push(tmp);
+    if (cur_cmd == "EMAIL_ADRESS" && !sta.empty()) {
         ret.push_back(std::pair<Information, std::stack<std::string> >(EMAIL_ADDRESS, sta));
-    } else if (cur_cmd == "IP_ADRESS") {
+    } else if (cur_cmd == "IP_ADRESS" && !sta.empty()) {
         ret.push_back(std::pair<Information, std::stack<std::string> >(IP_ADDRESS, sta));
-    } else if (cur_cmd == "PHONE_NUMBER") {
+    } else if (cur_cmd == "PHONE_NUMBER" && !sta.empty()) {
         ret.push_back(std::pair<Information, std::stack<std::string> >(PHONE_NUMBER, sta));
     }
     std::cout << "out : tmp = \"" << tmp << "\"" << std::endl << "cur_cmd = \"" << cur_cmd << "\"" << std::endl;
@@ -59,4 +98,5 @@ int	main()
     while (getline(std::cin, tmp)) {
         parse(tmp);
     }
+    return (0);
 }
